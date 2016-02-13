0f352232c212b6df344c768f55d72e16e36a31c3
Brett Henderson <bretth18@gmail.com>
2016-02-13 15:43:00 -0800

    sparkpost integration

--- emailAI/server/email/config.js
+++ emailAI/server/email/config.js
@@ -1,21 +1,58 @@
+// Meteor.startup(function() {
+//
+//   Meteor.Mailgun.config({
+//     username: 'postmaster@domain.com',
+//     password: 'password-goes-here'
+//   });
+//
+//   Meteor.methods({
+//     'sendContactEmail': function(name, email, message) {
+//       this.unblock();
+//
+//       Meteor.Mailgun.send({
+//         to: 'recipient@example.com',
+//         from: name + ' <' + email + '>',
+//         subject: 'New Contact Form Message',
+//         text: message,
+//         html: Handlebars.templates['contactEmail']({siteURL: Meteor.absoluteUrl(), fromName: name, fromEmail: email, message: message})
+//       });
+//     }
+//   });
+// });
+
+
+//handles email
 Meteor.startup(function() {
+  smtp = {
+    username:'postmaster@idbolt.io',
+    password:'email22',
+    server: 'smtp.sparkpostmail.com',
+    post: 587
+  }
+  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+});
 
-  Meteor.Mailgun.config({
-    username: 'postmaster@domain.com',
-    password: 'password-goes-here'
-  });
 
-  Meteor.methods({
-    'sendContactEmail': function(name, email, message) {
-      this.unblock();
+//sparkpost
+var SparkPost = Meteor.npmRequire('sparkpost');
+var sparky = new SparkPost('<YOUR API KEY>');
 
-      Meteor.Mailgun.send({
-        to: 'recipient@example.com',
-        from: name + ' <' + email + '>',
-        subject: 'New Contact Form Message',
-        text: message,
-        html: Handlebars.templates['contactEmail']({siteURL: Meteor.absoluteUrl(), fromName: name, fromEmail: email, message: message})
-      });
-    }
-  });
+sparky.transmissions.send({
+  transmissionBody: {
+    content: {
+      from: 'testing@sparkpostbox.com',
+      subject: 'Oh hey!',
+      html:'<html><body><p>Testing SparkPost - the world\'s most awesomest email service!</p></body></html>'
+    },
+    recipients: [
+      {address: 'developers@sparkpost.com'}
+    ]
+  }
+}, function(err, res) {
+  if (err) {
+    console.log('Whoops! Something went wrong');
+    console.log(err);
+  } else {
+    console.log('Woohoo! You just sent your first mailing!');
+  }
 });
--- emailAI/server/watson/watson.js
+++ emailAI/server/watson/watson.js
@@ -1,15 +1,15 @@
 var watson = Meteor.npmRequire('watson-developer-cloud');
 
 var tone_analyzer = watson.tone_analyzer({
-  username: '<username>',
-  password: '<password>',
+  username: '5d2c5f1e-fbe2-4aa6-8186-2c9a6dd9f9aa',
+  password: 'xPYobQo1AZdx',
   version: 'v3-beta',
   version_date: '2016-02-11'
 });
 
 
 //sparkpost input
-tone_analyzer.tone({ text: 'Greetings from Watson Developer Cloud!' },
+tone_analyzer.tone({ text: 'watson is stupid' },
   function(err, tone) {
     if (err)
       console.log(err);
