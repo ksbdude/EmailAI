705de92f57464f7cc9d9ea6fc74b364bf71715c9
ksbdude <kevinscottburns@gmail.com>
2016-02-14 11:06:39 -0800

    work

--- emailAI/server/watson/watson.js
+++ emailAI/server/watson/watson.js
@@ -36,44 +36,7 @@ tone_analyzer.tone({ text: 'text' },
       console.log(err);
     else {
       console.log(JSON.stringify(tone, null, 2));
-    }
-  
-      //watsonData = {JSON.stringify(tone, null, 2)};
-      //var arr = JSON.parse(response);
-  //     var i;
-  //     var out = "<table>";
-  //
-  //     for (name in JSON.document_tone.tone_categories.tones) {
-  //       tones = myJSONObject.document_tone.tone_categories.tones[name];
-  //       for (i = 0; i < tones.length; i++) {
-  //         out += "<tr><td>" +
-  //         tones[2]
-  //         "</td><td>" +
-  //         tones[0]
-  //        "</td><td>";
-  //       }
-  //     }
-  //
-  //     // for(i = 0; i < arr.length; i++) {
-  //     //   out += "<tr><td>" +
-  //     //   arr[i].document_tone +
-  //     //   "</td><td>" +
-  //     //   arr[i].tone_categories +
-  //     //   "</td><td>" +
-  //     //   arr[i].tones +
-  //     //   "</td></tr>";
-  //     // }
-  //   }
-  //   out += "</table>";
-  //   document.getElementById("id01").innerHTML = out;
-  //   console.log(out);
-  // });
-
-//
-// function parseToEmail(response) {
-//
-// }
-
+    }});
 
 function parseToEmail(response) {
 
