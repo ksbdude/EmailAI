7d29b662516024e954d71ea8937bc6f991d022c2
Brett Henderson <bretth18@gmail.com>
2016-02-14 11:02:32 -0800

    test

--- emailAI/server/watson/watson.js
+++ emailAI/server/watson/watson.js
@@ -5,7 +5,7 @@ var ref = new Firebase("https://watspark.firebaseio.com/raw-events/");
 ref.orderByKey().on("child_added", function(snapshot) {
   var testThing = snapshot.val();
   console.log(snapshot.exportVal());
-  var watsonInput = snapshot.exportVal();
+   watsonInput = snapshot.exportVal();
   return watsonInput;
 });
 
@@ -27,38 +27,40 @@ tone_analyzer.tone({ text: watsonInput },
       console.log(err);
     else {
       console.log(JSON.stringify(tone, null, 2));
+    }
+  );
       //watsonData = {JSON.stringify(tone, null, 2)};
       //var arr = JSON.parse(response);
-      var i;
-      var out = "<table>";
+  //     var i;
+  //     var out = "<table>";
+  //
+  //     for (name in JSON.document_tone.tone_categories.tones) {
+  //       tones = myJSONObject.document_tone.tone_categories.tones[name];
+  //       for (i = 0; i < tones.length; i++) {
+  //         out += "<tr><td>" +
+  //         tones[2]
+  //         "</td><td>" +
+  //         tones[0]
+  //        "</td><td>";
+  //       }
+  //     }
+  //
+  //     // for(i = 0; i < arr.length; i++) {
+  //     //   out += "<tr><td>" +
+  //     //   arr[i].document_tone +
+  //     //   "</td><td>" +
+  //     //   arr[i].tone_categories +
+  //     //   "</td><td>" +
+  //     //   arr[i].tones +
+  //     //   "</td></tr>";
+  //     // }
+  //   }
+  //   out += "</table>";
+  //   document.getElementById("id01").innerHTML = out;
+  //   console.log(out);
+  // });
 
-      for (name in JSON.document_tone.tone_categories.tones) {
-        tones = myJSONObject.document_tone.tone_categories.tones[name];
-        for (i = 0; i < tones.length; i++) {
-          out += "<tr><td>" +
-          tones[2]
-          "</td><td>" +
-          tones[0]
-         "</td><td>";
-        }
-      }
-
-      // for(i = 0; i < arr.length; i++) {
-      //   out += "<tr><td>" +
-      //   arr[i].document_tone +
-      //   "</td><td>" +
-      //   arr[i].tone_categories +
-      //   "</td><td>" +
-      //   arr[i].tones +
-      //   "</td></tr>";
-      // }
-    }
-    out += "</table>";
-    document.getElementById("id01").innerHTML = out;
-    console.log(out);
-  });
-
-
-function parseToEmail(response) {
-
-}
+//
+// function parseToEmail(response) {
+//
+// }
