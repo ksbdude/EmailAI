2c46487780211ad54dee7e8e36445c5e71bf2e73
ksbdude <kevinscottburns@gmail.com>
2016-02-14 11:01:15 -0800

    watson

--- emailAI/server/watson/watson.js
+++ emailAI/server/watson/watson.js
@@ -8,56 +8,16 @@ ref.orderByKey().on("child_added", function(snapshot) {
   return watsonInput;
 });
 
+//sparkpost input
+tone_analyzer.tone({ text: 'watson is really wattson' },
+  function(err, tone) {
+    if (err)
+      console.log(err);
+    else {
+      console.log(JSON.stringify(tone, null, 2));
+  }});
 
-// var watson = Meteor.npmRequire('watson-developer-cloud');
-//
-// var tone_analyzer = watson.tone_analyzer({
-//   username: '5d2c5f1e-fbe2-4aa6-8186-2c9a6dd9f9aa',
-//   password: 'xPYobQo1AZdx',
-//   version: 'v3-beta',
-//   version_date: '2016-02-11'
-// });
-//
-//
-// //sparkpost input
-// tone_analyzer.tone({ text: 'watson is really wattson' },
-//   function(err, tone) {
-//     if (err)
-//       console.log(err);
-//     else {
-//       console.log(JSON.stringify(tone, null, 2));
-//       //watsonData = {JSON.stringify(tone, null, 2)};
-//       //var arr = JSON.parse(response);
-//       var i;
-//       var out = "<table>";
-//
-//       for (name in JSON.document_tone.tone_categories.tones) {
-//         tones = myJSONObject.document_tone.tone_categories.tones[name];
-//         for (i = 0; i < tones.length; i++) {
-//           out += "<tr><td>" +
-//           tones[2]
-//           "</td><td>" +
-//           tones[0]
-//          "</td><td>";
-//         }
-//       }
-//
-//       // for(i = 0; i < arr.length; i++) {
-//       //   out += "<tr><td>" +
-//       //   arr[i].document_tone +
-//       //   "</td><td>" +
-//       //   arr[i].tone_categories +
-//       //   "</td><td>" +
-//       //   arr[i].tones +
-//       //   "</td></tr>";
-//       // }
-//     }
-//     out += "</table>";
-//     document.getElementById("id01").innerHTML = out;
-//     console.log(out);
-//   });
-//
-//
-// function parseToEmail(response) {
-//
-// }
+
+function parseToEmail(response) {
+
+}
