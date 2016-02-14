198d5d05b66bb537f0ea17f3bb957ca0d83fe4a9
ksbdude <kevinscottburns@gmail.com>
2016-02-13 19:19:12 -0800

    TABLE

--- emailAI/server/watson/watson.js
+++ emailAI/server/watson/watson.js
@@ -16,4 +16,24 @@ tone_analyzer.tone({ text: 'watson is stupid' },
     else
       console.log(JSON.stringify(tone, null, 2));
       watsonData = {JSON.stringify(tone, null, 2)};
+      var arr = JSON.parse(response);
+    var i;
+    var out = "<table>";
+
+    for(i = 0; i < arr.length; i++) {
+        out += "<tr><td>" +
+        arr[i].document_tone +
+        "</td><td>" +
+        arr[i].tone_categories +
+        "</td><td>" +
+        arr[i].tones +
+        "</td></tr>";
+    }
+    out += "</table>";
+    document.getElementById("id01").innerHTML = out;
 });
+
+
+function parseToEmail(response) {
+    
+}
