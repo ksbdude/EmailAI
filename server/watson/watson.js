9eb2b6f53d561ed137d0f90435c1b9a7503cfadb
Kevin Burns <kevinscottburns@gmail.com>
2016-02-16 10:49:19 -0800

    Cleaned Up

--- server/watson/watson.js
+++ server/watson/watson.js
@@ -4,10 +4,7 @@ var watsonInput = "text";
 
 var ref = new Firebase("https://watspark.firebaseio.com/raw-events/");
 
-//grabs only the last added key
 ref.orderByKey().limitToLast(1).on("child_added", function(snapshot) {
-  //var testThing = snapshot.val();
-
   var obj = JSON.stringify(snapshot.val());
   var jText = obj
 
@@ -33,7 +30,7 @@ ref.orderByKey().limitToLast(1).on("child_added", function(snapshot) {
             var SparkPost = Meteor.npmRequire('sparkpost');
     var sparky = new SparkPost('eb5e8fde469ca2f150b28539eeccee8100b99e1f');
     
-    var emailText = '<html><body><p>' + JSON.stringify(tone.document_tone.tone_categories.tones, null, 2) + ' </p></body></html>';
+    var emailText = '<html><body><h1>Original Email: </h1><p>' + watsonInput.text '</p> <h1>Results: </h1><p>' + JSON.stringify(tone.document_tone.tone_categories.tones, null, 2) + ' </p></body></html>';
     
     sparky.transmissions.send({
       transmissionBody: {
@@ -56,42 +53,4 @@ ref.orderByKey().limitToLast(1).on("child_added", function(snapshot) {
       }
     });
     }});
-
-    //sends email back to client
-
-
-
 });
-
-//sparkpost input
-// tone_analyzer.tone({ text: watsonInput },
-//   function(err, tone) {
-//     if (err)
-//       console.log(err);
-//     else {
-//       console.log(JSON.stringify(tone, null, 2));
-//   }});
-
-
-
-// var watson = Meteor.npmRequire('watson-developer-cloud');
-//
-// var tone_analyzer = watson.tone_analyzer({
-//   username: '5d2c5f1e-fbe2-4aa6-8186-2c9a6dd9f9aa',
-//   password: 'xPYobQo1AZdx',
-//   version: 'v3-beta',
-//   version_date: '2016-02-11'
-// });
-//
-// //sparkpost input
-// tone_analyzer.tone({ text : "" + watsonInput.text },
-//   function(err, tone) {
-//   	console.log("RUNNING WATSON");
-//     if (err)
-//       console.log(err);
-//     else {
-//       console.log(JSON.stringify(tone, null, 2));
-//   }});
-// function parseToEmail(response) {
-
-// }
