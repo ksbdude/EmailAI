cf5b85473fc880d2cf8f769d076accc986d3a93e
Brett Henderson <bretth18@gmail.com>
2016-02-18 18:40:36 -0800

    added language processing

--- server/watson/watson.js
+++ server/watson/watson.js
@@ -33,6 +33,40 @@ ref.orderByKey().limitToLast(1).on("child_added", function(snapshot) {
   });
 
 
+//lanaguage processing for multi-language translation from watson
+var language_translation = watson.language_translation({
+  //temp settings for testing
+  username: '5d2c5f1e-fbe2-4aa6-8186-2c9a6dd9f9aa',
+  password: 'xPYobQo1AZdx',
+  version: 'v2'
+});
+
+//identify the language to be passed into translation
+language_translation.identify({ text: "" + watsonInput },
+  function(err, identifiedTranslation){
+    if (err){
+      console.log('FAILED TO IDENTIFY LANGUAGE');
+    } else {
+      console.log(identifiedTranslation);
+    }
+  }
+})
+
+
+//handles our translation and passes to watsonInput
+// language_translation.translate({
+//   //
+//   //
+//   function(err, transle){
+//     if (err){
+//       console.log(err);
+//     } else {
+//       console.log(translate);
+//     }
+//   }
+// });
+
+//these should be stored in env.keys
 var tone_analyzer = watson.tone_analyzer({
   username: '5d2c5f1e-fbe2-4aa6-8186-2c9a6dd9f9aa',
   password: 'xPYobQo1AZdx',
@@ -50,15 +84,27 @@ tone_analyzer.tone({ text : "" + watsonInput},
     watsonOutput = JSON.stringify(tone, null, 2);
   }});
 
-// //JSON2HTML
-// var json2html = Meteor.npmRequire('node-json2html');
-// var transformer = {"tag":"li", "id":"${tone_name}", children:[
-//   {"tag":"span", "html":"${score}"}
-// ]};
-//
-// var jsonFormated = json2html.transform(watsonOutput,transformer);
-// //debugging, remove colors package
-// console.log(jsonFormated.rainbow);
+//JSON2HTML
+var json2html = Meteor.npmRequire('node-json2html');
+//transformer formats the JSON data into HTML
+var transformer = {"tag":"div","id":"${document_tone}","children":[
+    {"tag":"div","id":"${tone_categories}","children":[
+        {"tag":"div","id":"${tones}","children":[
+            {"tag":"span","html":" ${score}"},
+            {"tag":"span","html":"tone_id: ${tone_id}"},
+            {"tag":"span","html":"tone_name ${tone_name}"}
+          ]}
+      ]}
+  ]};
+
+//watson output formatted in HTML
+//var handleInput = Template.handleInput.onRendered = function(){
+
+//};
+
+var jsonFormated = json2html.transform(watsonOutput,transformer);
+//debugging, remove colors package
+console.log('JSON FORMATTED: ' + jsonFormated);
 
 emailText = '<html><body><h2>Original Email: </h2><p>' + watsonInput + '</p> <h2>Results: </h2><p>' + watsonOutput + ' </p></body></html>';
 console.log('EMAIL TEXT: '+ emailText);
