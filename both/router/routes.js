1e41358e97d3604b57f0c5cf1f95bb0da6f32597
Brett Henderson <bretth18@gmail.com>
2016-02-24 15:43:21 -0800

    front-end changes

--- both/router/routes.js
+++ both/router/routes.js
@@ -22,6 +22,11 @@ Router.route('/about', {
   name: 'about',
 });
 
+//routing for info page
+Router.route('/info', {
+  name: 'info',
+});
+
 //generate new route based on id
 Router.route('/:_id', {
   name: 'resultsAuth',
